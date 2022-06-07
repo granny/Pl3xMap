722b01c5afdc3cb17b4b2e8ed8259dcecbe14177
BillyGalbreath <blake.galbreath@gmail.com>
2022-06-07 07:21:11 -0500

    base project structure

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    `java-library`
+    id("io.papermc.paperweight.userdev") version "1.3.6"
+}
+
+group = "net.pl3x.map"
+version = "2.0.0-SNAPSHOT"
+description = "Minimalistic and lightweight world map viewer for Paper servers"
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+dependencies {
+    paperDevBundle("1.18.2-R0.1-SNAPSHOT")
+}
+
+tasks {
+    assemble {
+        dependsOn(reobfJar)
+    }
+
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+    javadoc {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+}
---
+++ src/main/java/net/pl3x/map/Pl3xMap.java
@@ -0,0 +1,15 @@
+package net.pl3x.map;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Pl3xMap extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        //
+    }
+
+    @Override
+    public void onDisable() {
+        //
+    }
+}
