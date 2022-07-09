0492d908c5a7e2a47a5558d31f7a0cfa8b132e5a
BillyGalbreath <blake.galbreath@gmail.com>
2022-07-09 00:32:05 -0500

    remove empty -dev jar after assemble

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,17 @@ subprojects {
 dependencies {
     paperDevBundle("1.19-R0.1-SNAPSHOT")
 }
+
+tasks {
+    // paperweight builds an empty -dev jar
+    // lets delete it to reduce confusion
+    assemble {
+        doLast {
+            delete(
+                fileTree("build/libs/") {
+                    include("**/*-dev.jar")
+                }
+            )
+        }
+    }
+}
