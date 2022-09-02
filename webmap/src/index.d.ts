221f27af6358a799db9efed5508e546fc205799b
James Lyne <jim+github@not-null.co.uk>
2022-09-02 15:24:54 +0100

    Ellipse typings

--- webmap/src/index.d.ts
+++ webmap/src/index.d.ts
@@ -1,4 +1,4 @@
-import {Layer} from "leaflet";
+import {LatLngExpression, Layer, PathOptions, Path, PointTuple, Point, LatLngBounds, LatLng} from "leaflet";
 import {World} from "./module/World";
 
 interface OverlayAddedPayload {
@@ -14,5 +14,18 @@ declare global {
         worldselected: CustomEvent<World>;
         rendererselected: CustomEvent<string>;
         overlayadded: CustomEvent<OverlayAddedPayload>;
+    }
+}
+
+module "leaflet" {
+    export function ellipse(latLng: LatLngExpression, radii: PointTuple, tilt: number, options: PathOptions): Ellipse;
+
+    interface Ellipse extends Path {
+        setRadius(radii: PointTuple): this;
+        getRadius(): Point;
+        setTilt(tilt: number): this;
+        getBounds(): LatLngBounds;
+        getLatLng(): LatLng;
+        setLatLng(latLng: LatLngExpression): this;
     }
 }
--- webmap/src/module/marker/Ellipse.ts
+++ webmap/src/module/marker/Ellipse.ts
@@ -1,9 +1,18 @@
 import * as L from "leaflet";
 import {MarkerOptions} from "./options/MarkerOptions";
 import {Marker} from "./Marker";
+import "../../lib/L.ellipse";
 
 export class Ellipse extends Marker {
     constructor(data: unknown[], options: MarkerOptions | undefined) {
-        super(L.circle([0, 0]));
+        const center = data[0] as L.LatLngExpression;
+        const radius = data[1] as L.PointTuple;
+        const tilt = data[3] as number;
+
+        const marker = L.ellipse(center, radius, tilt, {
+           ...options?.properties
+        });
+
+        super(marker);
     }
 }
