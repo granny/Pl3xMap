fe97788d9b956fad43a71a3477652353d61f1ab4
BillyGalbreath <blake.galbreath@gmail.com>
2022-09-14 09:39:46 -0500

    don't use addon's key for renderer key (capitalization issue)

--- addons/flowermap/src/main/java/net/pl3x/map/addon/flowermap/FlowerMap.java
+++ addons/flowermap/src/main/java/net/pl3x/map/addon/flowermap/FlowerMap.java
@@ -1,5 +1,6 @@
 package net.pl3x.map.addon.flowermap;
 
+import net.pl3x.map.Key;
 import net.pl3x.map.Pl3xMap;
 import net.pl3x.map.addon.Addon;
 import net.pl3x.map.addon.flowermap.renderer.FlowerMapRenderer;
@@ -7,17 +8,19 @@ import net.pl3x.map.render.RendererHolder;
 import net.pl3x.map.render.RendererRegistry;
 
 public class FlowerMap extends Addon {
+    public static final Key KEY = new Key("flowermap");
+
     @Override
     public void onEnable() {
         // register our custom renderer with Pl3xMap
         RendererRegistry registry = Pl3xMap.api().getRendererRegistry();
-        registry.register(new RendererHolder(getKey(), FlowerMapRenderer.class));
+        registry.register(new RendererHolder(KEY, FlowerMapRenderer.class));
     }
 
     @Override
     public void onDisable() {
         // unregister our custom renderer from Pl3xMap
         RendererRegistry registry = Pl3xMap.api().getRendererRegistry();
-        registry.unregister(getKey());
+        registry.unregister(KEY);
     }
 }
--- addons/inhabited/src/main/java/net/pl3x/map/addon/inhabited/Inhabited.java
+++ addons/inhabited/src/main/java/net/pl3x/map/addon/inhabited/Inhabited.java
@@ -1,5 +1,6 @@
 package net.pl3x.map.addon.inhabited;
 
+import net.pl3x.map.Key;
 import net.pl3x.map.Pl3xMap;
 import net.pl3x.map.addon.Addon;
 import net.pl3x.map.addon.inhabited.renderer.InhabitedRenderer;
@@ -7,17 +8,19 @@ import net.pl3x.map.render.RendererHolder;
 import net.pl3x.map.render.RendererRegistry;
 
 public class Inhabited extends Addon {
+    public static final Key KEY = new Key("inhabited");
+
     @Override
     public void onEnable() {
         // register our custom renderer with Pl3xMap
         RendererRegistry registry = Pl3xMap.api().getRendererRegistry();
-        registry.register(new RendererHolder(getKey(), InhabitedRenderer.class));
+        registry.register(new RendererHolder(KEY, InhabitedRenderer.class));
     }
 
     @Override
     public void onDisable() {
         // unregister our custom renderer from Pl3xMap
         RendererRegistry registry = Pl3xMap.api().getRendererRegistry();
-        registry.unregister(getKey());
+        registry.unregister(KEY);
     }
 }
