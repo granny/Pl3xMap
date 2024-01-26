06f6f317d443fe65b5276fe80a41f0d08e29f4f4
granny <contact@granny.dev>
2024-01-26 03:49:30 -0800

    add methods for setting task delay as ticks in scheduler

--- core/src/main/java/net/pl3x/map/core/scheduler/Scheduler.java
+++ core/src/main/java/net/pl3x/map/core/scheduler/Scheduler.java
@@ -99,12 +99,35 @@ public class Scheduler {
     /**
      * Add task to the scheduler.
      *
+     * @param delay    Delay (in seconds or ticks) before task starts
+     * @param ticks    Set to true to pass the delay as ticks instead of seconds
+     * @param runnable Task to add
+     */
+    public void addTask(int delay, @NotNull Runnable runnable, boolean ticks) {
+        addTask(delay, false, runnable, ticks);
+    }
+
+    /**
+     * Add task to the scheduler.
+     *
      * @param delay    Delay (in seconds) before task starts
      * @param repeat   Delay (in seconds) before task repeats
      * @param runnable Task to add
      */
     public void addTask(int delay, boolean repeat, @NotNull Runnable runnable) {
-        addTask(new Task(TickUtil.toTicks(delay), repeat) {
+        addTask(delay, repeat, runnable, false);
+    }
+
+    /**
+     * Add task to the scheduler.
+     *
+     * @param delay    Delay (in seconds or ticks) before task starts
+     * @param repeat   Delay (in seconds or ticks) before task repeats
+     * @param runnable Task to add
+     * @param ticks    Set to true to pass the delay as ticks instead of seconds
+     */
+    public void addTask(int delay, boolean repeat, @NotNull Runnable runnable, boolean ticks) {
+        addTask(new Task(ticks ? delay : TickUtil.toTicks(delay), repeat) {
             @Override
             public void run() {
                 runnable.run();
