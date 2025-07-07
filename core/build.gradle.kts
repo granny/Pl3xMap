fa9f3eee9422e4369c89f7d137e3dd652e8e0a0f
granny <contact@granny.dev>
2025-07-06 17:43:15 -0700

    use self-published adventure-bukkit

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -18,6 +18,7 @@ base {
 }
 
 repositories {
+    maven("https://repo.granny.dev/snapshots/")
     maven("https://oss.sonatype.org/content/repositories/snapshots/") {
         name = "oss-sonatype-snapshots"
         mavenContent {
--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -25,6 +25,7 @@ loom {
 }
 
 repositories {
+    maven("https://repo.granny.dev/snapshots/")
     maven("https://oss.sonatype.org/content/repositories/snapshots/") {
         name = "sonatype-snapshots"
         mavenContent {
