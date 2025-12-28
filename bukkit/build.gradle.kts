057a6d8081644a607e6236325f30f414473130e4
granny <contact@granny.dev>
2025-12-28 15:39:41 -0800

    add reobf support back

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -35,10 +35,10 @@ dependencies {
 }
 
 tasks {
-    /*reobfJar { // TODO: uncomment once paper has reobf support again
+    reobfJar {
         dependsOn(jar)
         outputJar.set(jar.get().archiveFile)
-    }*/ // TODO: uncomment once paper has reobf support again
+    }
 
     // needed for below jank
     compileJava {
@@ -52,13 +52,10 @@ tasks {
         manifest {
             from(project(":core").tasks.named<Jar>("shadowJar").get().manifest)
         }
-
-        archiveClassifier.set("") // TODO: delete once paper has reobf support again
     }
 
     build {
-        //dependsOn(reobfJar) // TODO: uncomment once paper has reobf support again
-        dependsOn(shadowJar) // TODO: delete once paper has reobf support again
+        dependsOn(reobfJar)
     }
 
     runServer {
