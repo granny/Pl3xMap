2a82b403459a4363bba6a03ec6e55445703d123c
granny <contact@granny.dev>
2026-03-20 16:18:02 -0700

    hide player if in world that is disabled

--- core/src/main/java/net/pl3x/map/core/player/PlayerRegistry.java
+++ core/src/main/java/net/pl3x/map/core/player/PlayerRegistry.java
@@ -115,6 +115,9 @@ public class PlayerRegistry extends Registry<Player> {
             if (PlayersLayerConfig.HIDE_INVISIBLE && player.isInvisible()) {
                 isHidden = true;
             }
+            if (player.getWorld().isEnabled()) {
+                isHidden = true;
+            }
 
             if (isHidden && !PlayersLayerConfig.SHOW_HIDDEN_PLAYERS_IN_SIDEBAR) {
                 return;
